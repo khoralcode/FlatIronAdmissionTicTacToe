d54fbe292950c8056a58f602085cb460c8e463b5
Alex K <khoralcode@gmail.com>
2014-10-16 11:19:05 -0700

    Initial Commit

---
+++ TicTacToe/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TicTacToe
+//
+//  Created by Alex on 10/16/14.
+//  Copyright (c) 2014 FlatIron. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
